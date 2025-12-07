61df15bfc04761a64a6f2f9e35253c307e1bfe3c
mr x -contributor <121688108+Shiv10122003@users.noreply.github.com>
2025-12-07 20:23:11 +0530

    Update API_URL to production endpoint

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/'
+const API_URL = 'https://blog-application-1-ji0j.onrender.com/api/'
 
 export const registerUser = async (userData)=>{
     return await axios.post(`${API_URL}auth/register`,userData);
